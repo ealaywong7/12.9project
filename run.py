bdd1e95f5943028832870e2c427a8118935ded34
ealaywong7 <166353700+ealaywong7@users.noreply.github.com>
2024-11-19 16:19:33 +0800

    Update run.py

--- run.py
+++ run.py
@@ -1,8 +1,6 @@
 from app import create_app
-from app.routes import api_bp
 
 app = create_app()
-app.register_blueprint(api_bp, url_prefix="/api")
 
 if __name__ == "__main__":
     app.run(debug=True)
