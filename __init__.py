447e0b3e0887de60b4f2cd623a73ef46bb40776a
ealaywong7 <166353700+ealaywong7@users.noreply.github.com>
2024-11-19 16:13:28 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,7 +1,12 @@
 from flask import Flask, session
+from .routes import api_bp  # 导入 routes.py 中定义的 Blueprint
 import os
 
 def create_app():
     app = Flask(__name__)
     app.secret_key = os.getenv("SECRET_KEY", "your_secret_key")  # 用于管理会话
+
+    # 注册 Blueprint
+    app.register_blueprint(api_bp, url_prefix='/api')  # 给 Blueprint 设置一个前缀
+
     return app
